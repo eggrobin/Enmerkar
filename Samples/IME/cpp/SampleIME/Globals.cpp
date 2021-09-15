18bffd7305d631df7a4a5eeb98141929cf2b842f
Robin Leroy <egg.robin.leroy@gmail.com>
2021-09-16 00:50:37 +0200

    Update all the GUIDs

--- Samples/IME/cpp/SampleIME/Globals.cpp
+++ Samples/IME/cpp/SampleIME/Globals.cpp
@@ -23,130 +23,84 @@ HFONT LatinFont;
 //---------------------------------------------------------------------
 // SampleIME CLSID
 //---------------------------------------------------------------------
-// {D2291A80-84D8-4641-9AB2-BDD1472C846B}
-extern const CLSID SampleIMECLSID = { 
-    0xd2291a80,
-    0x84d8,
-    0x4641,
-    { 0x9a, 0xb2, 0xbd, 0xd1, 0x47, 0x2c, 0x84, 0x6b }
-};
+// {F87CB858-5A61-42FF-98E4-CF3966457808}
+static const GUID SampleIMECLSID = 
+{ 0xf87cb858, 0x5a61, 0x42ff, { 0x98, 0xe4, 0xcf, 0x39, 0x66, 0x45, 0x78, 0x8 } };
+
 
 std::wstring trace = L"";
 
 //---------------------------------------------------------------------
 // Profile GUID
 //---------------------------------------------------------------------
-// {83955C0E-2C09-47a5-BCF3-F2B98E11EE8B}
-extern const GUID SampleIMEGuidProfile = { 
-    0x83955c0e,
-    0x2c09,
-    0x47a5,
-    { 0xbc, 0xf3, 0xf2, 0xb9, 0x8e, 0x11, 0xee, 0x8b }
-};
+// {5E81C0AA-9CC6-453C-B67D-FA70246C7EFC}
+static const GUID SampleIMEGuidProfile = 
+{ 0x5e81c0aa, 0x9cc6, 0x453c, { 0xb6, 0x7d, 0xfa, 0x70, 0x24, 0x6c, 0x7e, 0xfc } };
+
 
 //---------------------------------------------------------------------
 // PreserveKey GUID
 //---------------------------------------------------------------------
-// {4B62B54B-F828-43B5-9095-A96DF9CBDF38}
-extern const GUID SampleIMEGuidImeModePreserveKey = {
-    0x4b62b54b, 
-    0xf828, 
-    0x43b5, 
-    { 0x90, 0x95, 0xa9, 0x6d, 0xf9, 0xcb, 0xdf, 0x38 } 
-};
+// {58FF7AF2-2FE9-4A7C-86DA-F1CDA7584F9B}
+static const GUID SampleIMEGuidImeModePreserveKey = 
+{ 0x58ff7af2, 0x2fe9, 0x4a7c, { 0x86, 0xda, 0xf1, 0xcd, 0xa7, 0x58, 0x4f, 0x9b } };
 
-// {5A08D6C4-4563-4E46-8DDB-65E75C4E73A3}
-extern const GUID SampleIMEGuidDoubleSingleBytePreserveKey = {
-    0x5a08d6c4, 
-    0x4563, 
-    0x4e46, 
-    { 0x8d, 0xdb, 0x65, 0xe7, 0x5c, 0x4e, 0x73, 0xa3 } 
-};
 
-// {175F062E-B961-4AED-A3DF-59F78A02862D}
-extern const GUID SampleIMEGuidPunctuationPreserveKey = {
-    0x175f062e, 
-    0xb961, 
-    0x4aed, 
-    { 0xa3, 0xdf, 0x59, 0xf7, 0x8a, 0x2, 0x86, 0x2d } 
-};
+// {245891E4-DBFA-4F8E-8A38-9D31D684DD80}
+static const GUID SampleIMEGuidDoubleSingleBytePreserveKey = 
+{ 0x245891e4, 0xdbfa, 0x4f8e, { 0x8a, 0x38, 0x9d, 0x31, 0xd6, 0x84, 0xdd, 0x80 } };
+
+
+// {1505A67E-B27E-495B-9FE0-3E82D95AAD92}
+static const GUID SampleIMEGuidPunctuationPreserveKey = 
+{ 0x1505a67e, 0xb27e, 0x495b, { 0x9f, 0xe0, 0x3e, 0x82, 0xd9, 0x5a, 0xad, 0x92 } };
+
 
 //---------------------------------------------------------------------
 // Compartments
 //---------------------------------------------------------------------
-// {101011C5-CF72-4F0C-A515-153019593F10}
-extern const GUID SampleIMEGuidCompartmentDoubleSingleByte = {
-    0x101011c5,
-    0xcf72,
-    0x4f0c,
-    { 0xa5, 0x15, 0x15, 0x30, 0x19, 0x59, 0x3f, 0x10 }
-};
+// {4582DA5D-31FD-496C-A162-C1CE0C404116}
+static const GUID SampleIMEGuidCompartmentDoubleSingleByte = 
+{ 0x4582da5d, 0x31fd, 0x496c, { 0xa1, 0x62, 0xc1, 0xce, 0xc, 0x40, 0x41, 0x16 } };
 
-// {DD321BCC-A7F8-4561-9B61-9B3508C9BA97}
-extern const GUID SampleIMEGuidCompartmentPunctuation = {
-    0xdd321bcc,
-    0xa7f8,
-    0x4561,
-    { 0x9b, 0x61, 0x9b, 0x35, 0x8, 0xc9, 0xba, 0x97 }
-};
+
+// {EA3E5C1E-BAB6-4B16-B961-CA93AA4741CC}
+static const GUID SampleIMEGuidCompartmentPunctuation = 
+{ 0xea3e5c1e, 0xbab6, 0x4b16, { 0xb9, 0x61, 0xca, 0x93, 0xaa, 0x47, 0x41, 0xcc } };
 
 
 //---------------------------------------------------------------------
 // LanguageBars
 //---------------------------------------------------------------------
 
-// {89BE500C-9462-4070-9DB0-B467BB051327}
-extern const GUID SampleIMEGuidLangBarIMEMode = {
-    0x89be500c,
-    0x9462,
-    0x4070,
-    { 0x9d, 0xb0, 0xb4, 0x67, 0xbb, 0x5, 0x13, 0x27 }
-};
+// {73240B87-AAA7-47FC-AB9B-061BE7ABA8B1}
+static const GUID SampleIMEGuidLangBarIMEMode = 
+{ 0x73240b87, 0xaaa7, 0x47fc, { 0xab, 0x9b, 0x6, 0x1b, 0xe7, 0xab, 0xa8, 0xb1 } };
 
-// {6A11D9DE-46DB-455B-A257-2EB615746BF4}
-extern const GUID SampleIMEGuidLangBarDoubleSingleByte = {
-    0x6a11d9de,
-    0x46db,
-    0x455b,
-    { 0xa2, 0x57, 0x2e, 0xb6, 0x15, 0x74, 0x6b, 0xf4 }
-};
+// {9300628E-9F30-4AD0-96AA-0262AF974759}
+static const GUID SampleIMEGuidLangBarDoubleSingleByte = 
+{ 0x9300628e, 0x9f30, 0x4ad0, { 0x96, 0xaa, 0x2, 0x62, 0xaf, 0x97, 0x47, 0x59 } };
 
-// {F29C731A-A51E-49FB-8A3C-EE51752912E2}
-extern const GUID SampleIMEGuidLangBarPunctuation = {
-    0xf29c731a,
-    0xa51e,
-    0x49fb,
-    { 0x8a, 0x3c, 0xee, 0x51, 0x75, 0x29, 0x12, 0xe2 }
-};
+// {8C695C50-BA1C-4216-A9AB-0E55658535AC}
+static const GUID SampleIMEGuidLangBarPunctuation =
+{ 0x8c695c50, 0xba1c, 0x4216, { 0xa9, 0xab, 0xe, 0x55, 0x65, 0x85, 0x35, 0xac } };
 
-// {4C802E2C-8140-4436-A5E5-F7C544EBC9CD}
-extern const GUID SampleIMEGuidDisplayAttributeInput = {
-    0x4c802e2c,
-    0x8140,
-    0x4436,
-    { 0xa5, 0xe5, 0xf7, 0xc5, 0x44, 0xeb, 0xc9, 0xcd }
-};
+// {89D322BB-A521-4CF2-8EA0-70ADA1762055}
+static const GUID SampleIMEGuidDisplayAttributeInput = 
+{ 0x89d322bb, 0xa521, 0x4cf2, { 0x8e, 0xa0, 0x70, 0xad, 0xa1, 0x76, 0x20, 0x55 } };
 
-// {9A1CC683-F2A7-4701-9C6E-2DA69A5CD474}
-extern const GUID SampleIMEGuidDisplayAttributeConverted = {
-    0x9a1cc683,
-    0xf2a7,
-    0x4701,
-    { 0x9c, 0x6e, 0x2d, 0xa6, 0x9a, 0x5c, 0xd4, 0x74 }
-};
+// {EA471FFA-97BE-4FA2-8BC0-FEDCE2F73FF4}
+static const GUID SampleIMEGuidDisplayAttributeConverted = 
+{ 0xea471ffa, 0x97be, 0x4fa2, { 0x8b, 0xc0, 0xfe, 0xdc, 0xe2, 0xf7, 0x3f, 0xf4 } };
 
 
 //---------------------------------------------------------------------
 // UI element
 //---------------------------------------------------------------------
 
-// {84B0749F-8DE7-4732-907A-3BCB150A01A8}
-extern const GUID SampleIMEGuidCandUIElement = {
-    0x84b0749f,
-    0x8de7,
-    0x4732,
-    { 0x90, 0x7a, 0x3b, 0xcb, 0x15, 0xa, 0x1, 0xa8 }
-};
+// {8C343000-C0F1-432D-A7B1-5B63F9901F98}
+static const GUID SampleIMEGuidCandUIElement = 
+{ 0x8c343000, 0xc0f1, 0x432d, { 0xa7, 0xb1, 0x5b, 0x63, 0xf9, 0x90, 0x1f, 0x98 } };
 
 //---------------------------------------------------------------------
 // Unicode byte order mark
