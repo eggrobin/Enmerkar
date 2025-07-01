41a9633daf3475af15792a5fad2b472ca8203830
Robin Leroy <egg.robin.leroy@gmail.com>
2025-07-01 20:55:02 +0900

    Use the values; need to do something about the curved GEŠMINU

--- decompositions.py
+++ decompositions.py
@@ -3,8 +3,22 @@ import re
 
 import asl
 
+signs_by_value : dict[str, list[asl.Sign]] = defaultdict(list)
+for sign in asl.osl.signs:
+    if isinstance(sign, asl.Sign):
+        if sign.deprecated:
+            continue
+        for value in sign.values:
+            if value.deprecated:
+                continue
+            if value.language:
+                continue
+            if value.text in signs_by_value and "ₓ" not in value.text:
+                raise ValueError(f"Multiple signs with value {value.text}: {signs_by_value[value.text][0].names}, {sign.names}")
+            signs_by_value[value.text].append(sign)
+
 with open("decompositions.txt", "w", encoding="utf-8") as f:
-    mapping = defaultdict(list)
+    mapping: dict[str, list[list[str]]] = defaultdict(list)
     for name, forms in asl.osl.forms_by_name.items():
         xsux = [form.unicode_cuneiform.text
                 for form in forms
@@ -20,7 +34,8 @@ with open("decompositions.txt", "w", encoding="utf-8") as f:
             continue
         parts : list[str] = []
         for part in re.split(r"([|.×()&+%]|@(?:[cfgstv]|180)?)", name):
-            for form in asl.osl.forms_by_name.get(part) or []:
+            for form in (asl.osl.forms_by_name.get(part) or
+                         signs_by_value.get(part.lower()) or []):
                 if form.unicode_cuneiform:
                     parts.append(form.unicode_cuneiform.text)
                     break
@@ -28,7 +43,6 @@ with open("decompositions.txt", "w", encoding="utf-8") as f:
                 parts.append(part)
         mapping[xsux].append(parts)
     for xsux, decompositions in mapping.items():
-
         if any(''.join(part
                         for part in parts
                         if part not in "|.+") != xsux
