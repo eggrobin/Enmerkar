ab4e91d72e375edb753aea75e3caf7519c445b10
Robin Leroy <egg.robin.leroy@gmail.com>
2025-07-26 14:29:02 +0200

    Things I forgot to commit

--- decompositions.py
+++ decompositions.py
@@ -6,31 +6,40 @@ import asl
 with open("decompositions.txt", "w", encoding="utf-8") as f:
     mapping: dict[str, list[list[str]]] = defaultdict(list)
     sequence_mapping: dict[str, list[list[str]]] = defaultdict(list)
-    for name, forms in asl.osl.forms_by_name.items():
-        xsux = [form.unicode_cuneiform.text
-                for form in forms
-                if form.unicode_cuneiform]
-        if not xsux:
-            continue
-        if len(set(xsux)) > 1:
-            raise ValueError(name, xsux)
-        xsux = xsux[0]
-        if 'X' in xsux or 'x' in xsux:
-            continue
-        if "|" not in name and "@" not in name:
-            continue
-        parts : list[str] = []
-        for part in re.split(r"([|.×()&+%]|@(?:[cfgstv]|180)?)", name):
-            for form in (asl.osl.forms_by_name.get(part) or
-                         asl.signs_by_value.get(part.lower()) or
-                         asl.forms_by_list_number.get(part) or
-                         []):
-                if form.unicode_cuneiform:
-                    parts.append(form.unicode_cuneiform.text)
-                    break
-            else:
-                parts.append(part)
-        mapping[xsux].append(parts)
+    for forms in asl.osl.forms_by_name.values():
+        for form in forms:
+            if not form.unicode_cuneiform:
+                continue
+            xsux = form.unicode_cuneiform.text
+            xsux = xsux[0]
+            if 'X' in xsux or 'x' in xsux:
+                continue
+            decompositions_by_name : dict[str, list[str]] = {}
+            for name in form.names:
+                if "|" not in name and "@" not in name:
+                    continue
+                parts : list[str] = []
+                for part in re.split(r"([|.×()&+%]|@(?:[cfgstv]|180)?)", name):
+                    for part_form in (asl.osl.forms_by_name.get(part) or
+                                      asl.signs_by_value.get(part.lower()) or
+                                      asl.forms_by_list_number.get(part) or
+                                      []):
+                        if part_form.unicode_cuneiform:
+                            parts.append(part_form.unicode_cuneiform.text)
+                            break
+                    else:
+                        parts.append(part)
+                decompositions_by_name[name] = parts
+            if not decompositions_by_name:
+                continue
+            base_name_operators = ([part for part in decompositions_by_name[form.names[0]]
+                                         if all(ord(c) <= 0x7F for c in part)]
+                                   if form.names[0] in decompositions_by_name else [])
+            for name, decomposition in decompositions_by_name.items():
+                operators = [part for part in decomposition if all(ord(c) <= 0x7F for c in part)]
+                if len(operators) > len(base_name_operators):
+                    print(f"Alternate name {name} is more decomposed than {form.names[0]}")
+                    mapping[xsux].append(decomposition)
     for xsux, decompositions in mapping.items():
         if any(''.join(part
                         for part in parts
