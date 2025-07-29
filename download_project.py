e8e720fe39f7c2925651c7d9a0fa69432a2f0c83
Robin Leroy <egg.robin.leroy@gmail.com>
2025-07-29 21:58:04 +0200

    It might be useful to source-control these things

---
+++ download_project.py
@@ -0,0 +1,29 @@
+import json
+import os
+import sys
+import urllib.request
+import zipfile
+
+project = sys.argv[1]
+
+def get_oracc_json_from_zip(z: str):
+  if z in os.listdir():
+    print(f"Reusing downloaded {z}...")
+  else:
+    print(f"Downloading {z}...")
+    z, _ = urllib.request.urlretrieve(f"http://oracc.org/json/{z}", z)
+
+  with zipfile.ZipFile(z, 'r') as f:
+    f.extractall()
+
+get_oracc_json_from_zip(f"{project}.zip")
+
+with open(f"{project}/corpus.json", encoding="utf-8") as f:
+  corpus = json.loads(f.read())
+  subprojects : set[str] = set(corpus["proxies"].values())
+
+for subproject in subprojects:
+  if subproject in ("uet6",):
+    print(f"Skipping {subproject}")
+    continue
+  get_oracc_json_from_zip(subproject.replace("/", "-") + ".zip")
---
+++ dug.py
@@ -0,0 +1,121 @@
+import json
+import os
+import re
+from typing import Any
+
+import sys
+sys.path.append('C:/Users/robin/Projects/Enmerkar')
+import asl
+
+class Line:
+  def __init__(self, source: str, ref: str, label: str, xsux: str) -> None:
+    self.source = source
+    self.ref = ref
+    self.label = label
+    self.xsux = xsux
+
+  def __repr__(self) -> str:
+     return f"Line({repr(self.ref)}, {repr(self.label)}, {repr(self.xsux)})"
+
+  def __str__(self) -> str:
+     return f"{self.source}/{self.ref}\t{self.label}\t{self.xsux}"
+
+  def html_ref(self) -> str:
+     return f"<a href='{self.source}/{self.ref}'>{self.source}/{self.ref.split('.')[0]} {self.label}</a>"
+
+  def html(self) -> str:
+     return f"<tr><td>{self.html_ref()}</td><td class=xsux>{self.xsux}</td></tr>"
+
+def get_xsux(oracc_json: Any, source : str|None = None, result: list[Line]|None = None) -> list[Line]:
+  if result is None:
+     result = []
+  if not source:
+     if "source" not in oracc_json:
+        raise ValueError("Missing source", oracc_json)
+     source : str = oracc_json["source"]
+  if "type" in oracc_json and oracc_json["type"] == "line-start":
+    if "label" in oracc_json:
+      result.append(Line(source, oracc_json["ref"], oracc_json["label"], ""))
+    else:
+       print(f"*** Line with no label: {oracc_json['ref']}")
+  if "cdl" in oracc_json:
+     for node in oracc_json["cdl"]:
+        get_xsux(node, source, result)
+  if "f" in oracc_json:
+      get_xsux(oracc_json["f"], source, result)
+  if "gdl" in oracc_json:
+     for node in oracc_json["gdl"]:
+        get_xsux(node, source, result)
+  if "group" in oracc_json:
+     for node in oracc_json["group"]:
+        get_xsux(node, source, result)
+  if "seq" in oracc_json:
+     for node in oracc_json["seq"]:
+        get_xsux(node, source, result)
+  if "utf8" in oracc_json:
+     result[-1].xsux += oracc_json["utf8"]
+  return result
+
+
+all_lines : list[Line] = []
+
+def get_corpus_lines(directory: str):
+  global all_lines
+  for filename in os.listdir(directory):
+    if os.path.isdir(directory + "/" + filename):
+      if filename == "corpusjson":
+        files = os.listdir(directory + "/corpusjson")
+        print(f"{len(files)} texts in {directory}...")
+        for filename in files:
+          with open(directory + "/corpusjson/" + filename, encoding="utf-8") as f:
+            source = f.read()
+            if not source:
+               print(f"*** {filename} in {directory} is empty, skipping.")
+               continue
+            text = json.loads(source)
+            try:
+              all_lines += get_xsux(text)
+            except Exception:
+              print(f"*** Exception while reading {filename} in {directory}:")
+              raise
+      else:
+        get_corpus_lines(directory + "/" + filename)
+
+for top_level in ("atae", "riao", "rinap", "saao",
+                  "tcma",
+                  ):
+  get_corpus_lines(top_level)
+
+query = "𒈬𒅗"
+tail_regex = re.compile("")#"𒋗|𒈽")
+
+concordance : list[tuple[Line, str, str, str]] = []
+
+for line in all_lines:
+  if query in line.xsux:
+      start = 0
+      while query in line.xsux[start:]:
+        i = line.xsux.index(query, start)
+        if not tail_regex.search(line.xsux[i+len(query):]):
+           break
+        concordance.append((line, line.xsux[:i], line.xsux[i:i+len(query)], line.xsux[i+len(query):]))
+        start = i + 1
+
+xsux_to_mzl_number : dict[str, int] = {}
+
+for mzl_number, forms in asl.osl.forms_by_source[asl.osl.sources["MZL"]].items():
+  for form in forms:
+     if form.unicode_cuneiform:
+        xsux_to_mzl_number[form.unicode_cuneiform.text] = mzl_number.first
+
+concordance = sorted(concordance, key=lambda x: [xsux_to_mzl_number.get(sign, -1) for sign in x[-1]])
+
+with open("out.html", "w", encoding="utf-8") as f:
+  print("<head><style>.xsux {font-family: Assurbanipal} .pre {text-align: right} .query {color: red}</style></head>", file=f)
+  print("<body><table>", file=f)
+  for line, pre, query, post in concordance:
+    print(f"<tr><td>{line.html_ref()}</td>", file=f)
+    print(f"<td class='xsux pre'>{pre}</td>", file=f)
+    print(f"<td class='xsux query'>{query}</td>", file=f)
+    print(f"<td class='xsux post'>{post}</td>", file=f)
+  print("</table></body>", file=f)
