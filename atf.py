308353f61a49d950b1c1828f1486ef7dd0c97dc9
Robin Leroy <egg.robin.leroy@gmail.com>
2025-09-08 21:49:09 +0200

    Towards an LL atf parser

--- atf.py
+++ atf.py
@@ -6,6 +6,18 @@ VALUE_LETTER = re.compile(r"(?:sz|s,|t,|[abdeghiklmnpqrstuvwyz'šṣṭʾŋ])",)
 VALUE_INDEX = re.compile(r"(?:[0-9₀-₉]+|x|ₓ)")
 VALUE = re.compile(fr"(?:{VALUE_LETTER.pattern}+{VALUE_INDEX.pattern}?)")
 
+class Extension(enum.Enum):
+  # https://oracc.museum.upenn.edu/ns/gdl/1.0/gdltut.html#Words
+  # In ATF words are separated by spaces, and graphemes within words are joined
+  # by hyphens. Note that periods (.) are only permitted inside compound
+  # graphemes.
+  DOT_AS_DELIMITER = 0,
+  UNICODE = 1,
+  # SAAo em dash, represented as --.
+  EM_DASH = 2,
+  # + between separate ligatured graphemes.
+  PLUS_AS_DELIMITER = 3,
+  DIŠLESS_NUMBERS = 4,
 
 NAME_LETTER = re.compile(r"(?:SZ|S,|T,|[ABDEGHIKLMNPQRSTUVWYZ'ŠṢṬʾŊ])")
 NAME_INDEX = re.compile(r"(?:[0-9₀-₉]+|X|ₓ)")
@@ -30,6 +42,8 @@ QUALIFIED =  re.compile(
   re.VERBOSE
 )
 
+DIŠLESS_NUMBER = re.compile(r"[0-5]?[0-9]")
+
 NUMBER = re.compile(
   fr"""(?:
       (?:n|[\d/]+)
@@ -38,11 +52,10 @@ NUMBER = re.compile(
           |{NAME.pattern}
           |{COMPOUND.pattern})
       \)
+    | {DIŠLESS_NUMBER.pattern}
   )""",
   re.VERBOSE
 )
-
-DIŠLESS_NUMBER = re.compile(r"[0-5]?[0-9]")
 
 # TODO(egg): Should a value really be allowed as a correction?
 ALTERNATIVE = re.compile(
@@ -80,8 +93,6 @@ PUNCTUATION = re.compile(
 ASCII_DIGRAPHS = re.compile(r"(sz|s,|t,|SZ|S,|T,)")
 NON_ASCII = re.compile(r"[^\x00-\x7F]")
 
-print(GRAPHEME.pattern)
-
 class SpanAttribute(enum.Enum):
   LOGOGRAM = 0, "_", "_"
   MAYBE = 1, "(", ")"
@@ -99,6 +110,38 @@ class SpanAttribute(enum.Enum):
 
   def __str__(self) -> str:
     return self.name
+
+INTRAWORD_DELIMITER = re.compile("[-:.+]")
+SHIFT = re.compile("%[a-z/-]+")
+COMMENT = re.compile(r"\(\#(?:[^#]|\#[^)])*\#\)|\(\$(?:[^$]|\$[^)])*\$\)")
+
+class Token:
+  def __init__(self, text: str, after: int):
+    self.text = text
+    self.after = after
+
+class Grapheme(Token):
+  pass
+
+class Comment(Token):
+  pass
+
+class Delimiter(Token):
+  pass
+
+class FieldSeparator(Token):
+  pass
+
+class Bracket(Token):
+  pass
+
+TOKENS : list[tuple[re.Pattern[str], type[Token]]] = [
+  (COMMENT, Comment),
+  (GRAPHEME, Grapheme),
+  (re.compile(" "), Delimiter),
+  (INTRAWORD_DELIMITER, Delimiter),
+] + [(re.compile(re.escape(c)), FieldSeparator) for c in ("#", '"', "~", "|", "=", "^", "@", "&")
+] + [(re.compile(re.escape(t)), Bracket) for a in SpanAttribute for t in ((a.open, a.close) if a.open != a.close else (a.open,))]
 
 # https://oracc.org/doc/help/editinginatf/primer/inlinetutorial/index.html#h_languages
 INLINE_LANGUAGE_CODES = {
@@ -118,148 +161,97 @@ INLINE_LANGUAGE_CODES = {
   "e"    : "sux-x-emesal",
 }
 
-class Extension(enum.Enum):
-  # https://oracc.museum.upenn.edu/ns/gdl/1.0/gdltut.html#Words
-  # In ATF words are separated by spaces, and graphemes within words are joined
-  # by hyphens. Note that periods (.) are only permitted inside compound
-  # graphemes.
-  DOT_AS_DELIMITER = 0,
-  UNICODE = 1,
-  # SAAo em dash, represented as --.
-  EM_DASH = 2,
-  # + between separate ligatured graphemes.
-  PLUS_AS_DELIMITER = 3,
-  DIŠLESS_NUMBERS = 4,
+class Lexer:
+  def __init__(self, source: str):
+    self.source = source
+    self.i = 0
+    self.ahead = None
 
-def parse_transliteration(source: str, language: str, extensions: set[Extension] = set()):
-  graphemes : list[tuple[str, str, set[SpanAttribute], str|None]] = []
-  i = 0
-  after_delimiter = " "
-  delimiter_before_determinative = None
-  delimiter_before_linguistic_gloss = None
-  attribute_run_lengths : dict[SpanAttribute, int] = {}
-  def start_span(attribute: SpanAttribute):
-    if attribute in attribute_run_lengths:
-      raise SyntaxError(f"Nested {attribute}: {source[:i]}☞{source[i:]}")
-    attribute_run_lengths[attribute] = 0
-  def end_span(attribute: SpanAttribute):
-    if attribute not in attribute_run_lengths:
-      raise SyntaxError(f"Unstarted {attribute}: {source[:i]}☞{source[i:]}")
-    del attribute_run_lengths[attribute]
-  while i < len(source):
-    while source[i] == " ":
-      i += 1
-      after_delimiter = " "
-    match = GRAPHEME.match(source, i)
-    if match:
-      if Extension.UNICODE in extensions and ASCII_DIGRAPHS.search(match.group()):
-        raise SyntaxError(f"ASCII digraphs: {source[:i]}☞{source[i:]}")
-      if Extension.UNICODE not in extensions and NON_ASCII.search(match.group()):
-        raise SyntaxError(f"Non-ASCII grapheme: {source[:i]}☞{source[i:]}")
-      if (not after_delimiter and
-          attribute_run_lengths.get(SpanAttribute.DETERMINATIVE) != 0 and
-          attribute_run_lengths.get(SpanAttribute.IMPLIED) != 0 and
-          attribute_run_lengths.get(SpanAttribute.LINGUISTIC_GLOSS) != 0):
-        raise SyntaxError(f"Missing delimiter: {source[:i]}☞{source[i:]}")
-      graphemes.append((match.group(), language, set(attribute_run_lengths), after_delimiter))
-      i = match.end()
-      after_delimiter = None
-      continue
-    match = DIŠLESS_NUMBER.match(source, i)
-    if match:
-      i = match.end()
-      after_delimiter = None
-      continue
-    if source.startswith("...", i):
-      if SpanAttribute.BROKEN not in attribute_run_lengths:
-        raise SyntaxError(f"... outside breakage brackets: {source[:i]}☞{source[i:]}")
-      after_delimiter = None
-      i += 3
-      continue
-    if Extension.EM_DASH in extensions and source.startswith("--", i):
-      if after_delimiter:
-        raise SyntaxError(f"Double delimiter: {source[:i]}☞{source[i:]}")
-      after_delimiter = "--"
-      i += 2
-      continue
-    if (source[i] in ("-", ":") or
-        Extension.DOT_AS_DELIMITER in extensions and source[i] == "." or
-        Extension.PLUS_AS_DELIMITER in extensions and source[i] == "+") :
-      if after_delimiter:
-        raise SyntaxError(f"Double delimiter: {source[:i]}☞{source[i:]}")
-      after_delimiter = source[i]
-      i += 1
-      continue
-    if source[i] == ";":
-      # Newline in case, see http://oracc.org/ns/gdl/1.0/gdltut.html#Intrusions.
-      i += 1
-      continue
-    if source[i] == "_":
-      if after_delimiter:
-        start_span(SpanAttribute.LOGOGRAM)
-      else:
-        end_span(SpanAttribute.LOGOGRAM)
-      i += 1
-      continue
-    match = PUNCTUATION.match(source, i)
-    if match:
-      i = match.end()
-      continue
-    if (source.startswith(
-      ("# ", '" ', "~ ", "| ", "= ", "^ ", "@ ", "& "), i) and
-      (i == 0 or source[i-1] == " ")):
-      # Column in lexical text.
-      i += 2
-      continue
-    if source[i] == "%":
-      inline_code = source[i+1:].split(" ", maxsplit=1)[0]
-      if inline_code not in INLINE_LANGUAGE_CODES and inline_code not in INLINE_LANGUAGE_CODES.values():
-        raise NameError(f"Unknown inline language code: {source[:i]}☞{source[i:]}", name=inline_code)
-      i += len(inline_code) + 2
-      after_delimiter = " "
-      language = INLINE_LANGUAGE_CODES.get(inline_code, inline_code)
-      continue
-    if source.startswith("($", i):
-      i = source.index("$)", i + 2) + 2
-      continue
-    for attribute in (
-        SpanAttribute.DOCUMENT_GLOSS,
-        SpanAttribute.IMPLIED,
-        SpanAttribute.EXCISED,
-        SpanAttribute.SUPPLIED,
-        SpanAttribute.MAYBE,
-        SpanAttribute.BROKEN):
-      if source.startswith(attribute.open, i):
-        start_span(attribute)
-        i += len(attribute.open)
+  def accept(self, token: type[Token], text: str|None = None):
+    ahead = self.lookahead()
+    if isinstance(ahead, token) and (
+        text is None or ahead.text == text):
+      self.advance()
+      return True
+    return False
+
+  def lookahead(self) -> Token|None:
+    if self.ahead is None:
+      self.ahead = self._next()
+    if self.ahead:
+      return self.ahead
+
+  def advance(self):
+    self.lookahead()
+    if self.ahead:
+      self.i = self.ahead.after
+    self.ahead = None
+
+  def _next(self) -> Token|None:
+    if self.i == len(self.source):
+      return None
+    matches : list[tuple[re.Match[str], type[Token]]] = []
+    for pattern, t in TOKENS:
+      match = pattern.match(self.source, self.i)
+      if match:
+        matches.append((match, t))
+    if not matches:
+      self.raise_syntax_error(f"Could not recognize token")
+    if len(matches) > 1:
+      matches = sorted(matches, key=lambda mt: -len(mt[0].group(0)))
+      #print(f"Multiple matching tokens {', '.join(f'{token.__name__} {match.group(0)}' for match, token in matches)}: {self.source[:self.i]}☞{self.source[self.i:]}")
+    match, t = matches[0]
+    return t(match.group(), match.end())
+
+  def raise_syntax_error(self, message: str):
+    raise SyntaxError(f"{message}: {self.source[:self.i]}☞{self.source[self.i:]}")
+
+def parse_transliteration(source: str):
+  lexer = Lexer(source)
+  while True:
+    parse_word(lexer)
+    if not lexer.accept(Delimiter, " "):
+      if lexer.lookahead() is None:
         break
-      elif source.startswith(attribute.close, i):
-        end_span(attribute)
-        i += len(attribute.close)
-        break
-    else:
-      if source.startswith(SpanAttribute.LINGUISTIC_GLOSS.open, i):
-        start_span(SpanAttribute.LINGUISTIC_GLOSS)
-        delimiter_before_linguistic_gloss = after_delimiter
-        i += len(SpanAttribute.LINGUISTIC_GLOSS.open)
-      elif source.startswith(SpanAttribute.LINGUISTIC_GLOSS.close, i):
-        end_span(SpanAttribute.LINGUISTIC_GLOSS)
-        after_delimiter = delimiter_before_linguistic_gloss
-        i += len(SpanAttribute.LINGUISTIC_GLOSS.close)
-      elif source[i] == "{":
-        start_span(SpanAttribute.DETERMINATIVE)
-        delimiter_before_determinative = after_delimiter
-        i += 1
-        if source[i] == "+":
-          # TODO(egg): Representation for phonetic complements?
-          i += 1
-      elif source[i] == "}":
-        end_span(SpanAttribute.DETERMINATIVE)
-        after_delimiter = delimiter_before_determinative
-        i += 1
-      else:
-        raise SyntaxError(f"Syntax error: {source[:i]}☞{source[i:]}")
-  return graphemes
+      lexer.raise_syntax_error("Expected space between words")
+    continue
+
+def parse_word(lexer: Lexer):
+  parse_decorated_grapheme(lexer)
+  while lexer.accept(Delimiter, "-"):
+    parse_decorated_grapheme(lexer)
+
+def parse_decorated_grapheme(lexer: Lexer):
+  parse_textual_span_openings(lexer)
+  parse_optional_determinative(lexer)
+  if not lexer.accept(Grapheme):
+    lexer.raise_syntax_error("Expected Grapheme")
+  parse_optional_determinative(lexer)
+  parse_textual_span_closings(lexer)
+
+def parse_optional_determinative(lexer: Lexer):
+  if lexer.accept(Bracket, "{"):
+    if not lexer.accept(Grapheme):
+      lexer.raise_syntax_error("Expected Grapheme")
+    while lexer.accept(Delimiter, "-"):
+      if not lexer.accept(Grapheme):
+        lexer.raise_syntax_error("Expected Grapheme")
+    if not lexer.accept(Bracket, "}"):
+      lexer.raise_syntax_error("Expected }")
+
+def parse_textual_span_openings(lexer: Lexer):
+  while True:
+    for bracket in ("_", "[", "(", "<", "<<"):
+      if lexer.accept(Bracket, bracket):
+        continue
+    break
+
+def parse_textual_span_closings(lexer: Lexer):
+  while True:
+    for bracket in ("_", "]", ")", ">", ">>"):
+      if lexer.accept(Bracket, bracket):
+        continue
+    break
 
 def get_base(value: str) -> str:
   if "-" in value:
@@ -295,95 +287,3 @@ def get_vowel(syllable_value: str, *args: None):
     return next((v for v in base if is_vowel(v)), *args)
   vowel, = (v for v in base if is_vowel(v))
   return vowel
-
-def get_value_counts(file: str, target_language: str, exclude: set[SpanAttribute]):
-  with open(file, encoding="utf-8") as f:
-    lines = f.readlines()
-  with open(file + ".log", mode="w", encoding="utf-8") as log:
-    occurrences : dict[str, list[str]] = defaultdict(list)
-
-    erroneous_texts : dict[str, list[str]] = defaultdict(list)
-
-    artefact = ""
-    language = "und"
-
-    for line in lines:
-      if not line.strip():
-        continue
-      if line.startswith("&"):
-        artefact = line.split("=")[0][1:].strip()
-        language = "und"
-        continue
-      elif line.startswith("#"):
-        if line.startswith("#atf: lang"):
-          language = line.split()[-1]
-        continue
-      elif line.startswith(("@", "$", "|", ">")):
-        continue
-      if " " not in line:
-        print("*** No space in", repr(line), file=log)
-        continue
-      number, text = line.split(" ", 1)
-      if not number.endswith("."):
-        print("*** Bad line number", repr(number), "in", line,
-              file=log)
-      text = text.strip()
-      try:
-        graphemes = parse_transliteration(text, language)
-      except (SyntaxError, NameError) as e:
-        print("***", str(e), file=log)
-        error_title = str(e).split(":")[0]
-        erroneous_texts[error_title].append(artefact)
-        continue
-      previous_graphemes : list[str] = []
-      for grapheme, grapheme_language, attributes, after_delimiter in graphemes:
-        if after_delimiter != "-":
-          previous_graphemes = []
-        grapheme = grapheme.rstrip("#?!*")
-        if not grapheme.strip():
-          previous_graphemes = []
-          continue
-
-        if any(c.isupper() for c in grapheme):
-          previous_graphemes = []
-          continue
-        if "/" in grapheme or "(" in grapheme:
-          previous_graphemes = []
-          continue
-        if grapheme in ("x", "n"):
-          previous_graphemes = []
-          continue
-        if grapheme_language != target_language:
-          previous_graphemes = []
-          continue
-        if any(attribute in exclude for attribute in attributes):
-          previous_graphemes = []
-          continue
-        grapheme = grapheme.replace("sz", "š").replace("s,", "ṣ").replace("t,", "ṭ")
-        grapheme = grapheme.replace(
-            "0", "₀").replace(
-            "1", "₁").replace(
-            "2", "₂").replace(
-            "3", "₃").replace(
-            "4", "₄").replace(
-            "5", "₅").replace(
-            "6", "₆").replace(
-            "7", "₇").replace(
-            "8", "₈").replace(
-            "9", "₉")
-        occurrences[grapheme].append(artefact)
-        if previous_graphemes and is_vc(grapheme):
-          this_vowel = get_vowel(grapheme)
-          previous_grapheme = previous_graphemes[-1]
-          if get_vowel(previous_grapheme, None) == this_vowel:
-            if is_cv(previous_grapheme):
-              occurrences[previous_grapheme + "-" + grapheme].append(artefact)
-            elif is_v(previous_grapheme) and len(previous_graphemes) >= 2:
-              if is_cv(previous_graphemes[-2]) and get_vowel(previous_graphemes[-2]) == this_vowel:
-                occurrences[previous_graphemes[-2] + "-" + previous_grapheme + "-" + grapheme].append(artefact)
-
-        previous_graphemes.append(grapheme)
-
-    for error_title, error_occurrences in erroneous_texts.items():
-      print(f"*** {error_title}: {len(error_occurrences)} in {len(set(error_occurrences))} texts")
-    return occurrences
--- oracc_atf_test.py
+++ oracc_atf_test.py
@@ -1,48 +1,54 @@
 import atf
 import re
 
-with open("oracc-atf/saao/saa01/SAA01_01.atf", encoding="utf-8") as f:
-  lines = f.readlines()
+for filename, extensions in (
+  ("oracc-atf/saao/saa01/SAA01_01.atf",
+   {atf.Extension.EM_DASH, atf.Extension.DOT_AS_DELIMITER, atf.Extension.PLUS_AS_DELIMITER}),
+  ("cdli/OB akk.atf", set())):
+  with open(filename, encoding="utf-8") as f:
+    lines = f.readlines()
 
-artefact = ""
-language = "und"
-in_translation = False
-
-for line in lines:
-  if not line.strip():
-    continue
-  if line.startswith("&"):
-    artefact = line.split("=")[0][1:].strip()
+    artefact = ""
     language = "und"
     in_translation = False
-    continue
-  elif line.startswith("#"):
-    if line.startswith("#atf: lang"):
-      language = line.split()[-1]
-    continue
-  elif line.startswith(("@", "$", "|", ">")):
-    if line.startswith("@translation"):
-      in_translation = True
-    continue
-  if in_translation:
-    continue
-  if line.startswith("==%"):
-    text = line[2:]
-  else:
-    # https://oracc.org/doc/help/editinginatf/primer/structuretutorial/index.html#h_textlines
-    # says space, but tabs occur even in
-    # https://oracc.org/doc/help/editinginatf/primer/index.html.
-    if " " not in line and "\t" not in line:
-      print("*** No space in", repr(line))
-      continue
-    number, text = re.split(r"[ \t]", line, maxsplit=1)
-    if not number.endswith("."):
-      print("*** Bad line number", repr(number), "in", line)
-  text = text.strip()
-  try:
-    graphemes = atf.parse_transliteration(
-      text, language,
-      {atf.Extension.EM_DASH, atf.Extension.DOT_AS_DELIMITER, atf.Extension.PLUS_AS_DELIMITER})
-  except (SyntaxError, NameError) as e:
-    print("***", artefact, e)
-    continue  
+
+    for line in lines:
+      if not line.strip():
+        continue
+      if line.startswith("&"):
+        artefact = line.split("=")[0][1:].strip()
+        language = "und"
+        in_translation = False
+        continue
+      elif line.startswith("#"):
+        if line.startswith("#atf: lang"):
+          language = line.split()[-1]
+        continue
+      elif line.startswith(("@", "$", "|", ">")):
+        if line.startswith("@translation"):
+          in_translation = True
+        continue
+      if in_translation:
+        continue
+      if line.startswith("==%"):
+        text = line[2:]
+      else:
+        # https://oracc.org/doc/help/editinginatf/primer/structuretutorial/index.html#h_textlines
+        # says space, but tabs occur even in
+        # https://oracc.org/doc/help/editinginatf/primer/index.html.
+        if " " not in line and "\t" not in line:
+          print("*** No space in", repr(line))
+          continue
+        number, text = re.split(r"[ \t]", line, maxsplit=1)
+        if not number.endswith("."):
+          print("*** Bad line number", repr(number), "in", line)
+      text = text.strip()
+      try:
+        graphemes = atf.parse_transliteration(text)
+      except (SyntaxError, NameError) as e:
+        print("***", artefact, e)
+        continue  
+
+INVALID_LINES = """
+1. 1(asz@c) {gesz}kabx(|KAxMASZ)? e2-an-da
+"""
